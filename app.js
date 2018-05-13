631f74889f438a2bccb26a57867916bb9afc345e
Olesia Davniuk <lv250385sov@gmail.com>
2018-05-13 20:33:55 +0300

    formatted code

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 angular.module('InputApp', [])
-	.controller('InputAppCtrl',InputAppCtrl);
+  .controller('InputAppCtrl', InputAppCtrl);
 
-InputAppCtrl.$inject = ['$scope'];	
+InputAppCtrl.$inject = ['$scope'];
 
 function InputAppCtrl($scope) {
-	$scope.enter = "Sign in!";
-	
-	$scope.warning = function(mes) {
-		return 'The field ' + mes + ' is required';
-	}
+  $scope.enter = "Sign in!";
 
-	$scope.submitForm = function() {
-		if ($scope.userForm.$valid) {
-			alert('You did everything right! You are  guru!');
-			this.user = {};
-		}
-	}
+  $scope.warning = function (mes) {
+    return 'The field ' + mes + ' is required';
+  }
+
+  $scope.submitForm = function () {
+    if ($scope.userForm.$valid) {
+      alert('You did everything right! You are  guru!');
+      this.user = {};
+    }
+  }
 }
