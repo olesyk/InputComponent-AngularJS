efc515a2a8cfa2c889487f989268e7609a2750f3
Olesia Davniuk <lv250385sov@gmail.com>
2018-05-13 20:27:32 +0300

    add files

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('InputApp', [])
+	.controller('InputAppCtrl',InputAppCtrl);
+
+InputAppCtrl.$inject = ['$scope'];	
+
+function InputAppCtrl($scope) {
+	$scope.enter = "Sign in!";
+	
+	$scope.warning = function(mes) {
+		return 'The field ' + mes + ' is required';
+	}
+
+	$scope.submitForm = function() {
+		if ($scope.userForm.$valid) {
+			alert('You did everything right! You are  guru!');
+			this.user = {};
+		}
+	}
+}
---
+++ inputField.controller.js
@@ -0,0 +1,39 @@
+InputFieldController.$inject = ['$scope'];
+
+function InputFieldController($scope) {
+	var self = this;
+   	this.$onInit = function() {
+    	this.model.$render = () => {
+        	this.value = this.model.$viewValue;
+       }
+    }
+
+	this.onChange = function() {
+        if (this.value == "") {
+            this.hasError = true;
+            this.model.$setValidity("required",false);
+        } else {
+            this.hasError = false;
+            this.model.$setValidity("required",true);
+        }
+            this.model.$setViewValue(this.value);
+	    }
+}
+
+function inputFieldComponent() {
+	return {
+        templateUrl: 'inputField.html',
+        controller: InputFieldController,
+		require: {
+            model: "ngModel"
+        },
+        bindings: {
+			id: "@",
+            requiredWarning: "&",
+			type:'@'
+        }
+    }
+}
+
+angular.module('InputApp')
+	.component('inputField', inputFieldComponent());
